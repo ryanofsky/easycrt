27925eb3598351ac2bb3d8101476f28e3ddcefaf
Russell Yanofsky <russ@yanofsky.org>
1997-12-02 01:31:52 +0000

    - added font for new demo

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@
 copy a:\easycrt\*.* c:\tpw\doc
 copy a:\easycrt\system\*.* c:\windows < a:\easycrt\setup.scp
 copy a:\easycrt\system\*.* c:\tpw     < a:\easycrt\setup.scp
+copy a:\easycrt\system\*.ttf c:\windows\fonts
 
 echo.
 echo EasyCRT has been installed.
