2a97ca351f9d1d3a9cc0a3b74b3e128e603b3319
Russell Yanofsky <russ@yanofsky.org>
1997-12-25 20:35:50 +0000

    - made some new sample programs

---
+++ samples/emouse.pas
@@ -0,0 +1,10 @@
+uses easycrt;
+
+begin
+  gotoxy(20,9); writeln('X':5,'   ','Y':5,'   ','LEFT':5,'   ','RIGHT':5);
+  repeat
+    gotoxy(20,10); writeln(mousex:5,'   ',mousey:5,'   ',ldown:5,'   ',rdown:5);
+    if ldown then pset(mousex,mousey,color[4]);
+    if rdown then pset(mousex,mousey,color[1]);
+  until inkey<>0;
+end.
