d9e1a8ed2bc7a5adfb2df84ea7342502416c7218
Russell Yanofsky <russ@yanofsky.org>
1998-01-27 01:11:42 +0000

    - deleted installation files

--- setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-copy a:\easycrt\*.* c:\tpw\doc
-copy a:\easycrt\system\*.* c:\windows < a:\easycrt\setup.scp
-copy a:\easycrt\system\*.* c:\tpw     < a:\easycrt\setup.scp
-copy a:\easycrt\system\*.ttf c:\windows\fonts
-
-echo.
-echo EasyCRT has been installed.
