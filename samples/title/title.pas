5bc1f28f8a91e9a335a58742520ce04936b78444
Russell Yanofsky <russ@yanofsky.org>
1998-11-25 05:10:46 +0000

    - animates new easycrt logo

---
+++ samples/title/title.pas
@@ -0,0 +1,142 @@
+uses easycrt,easygdi;
+
+const overlap = 10;
+const maxheight = 300;
+
+FUNCTION PwrXY (X, Y : real) : real; (* raise x to the power y           *)
+BEGIN
+  IF (Y = 0.0) THEN
+    PwrXY := 1.0
+  ELSE
+    IF (X <= 0.0) AND (Frac(Y) = 0.0) THEN
+      IF (Frac(Y / 2)) = 0.0 THEN
+        PwrXY := Exp(Y * Ln(Abs(X)))
+      ELSE
+        PwrXY := - Exp(Y * Ln(Abs(X)))
+    ELSE
+      PwrXY := Exp(Y * Ln(X));
+END;
+
+
+procedure easyspiral;
+  var i,j,centerx,centery,x,y:integer;
+      angle,radius: real;
+      
+  begin
+
+    setbrush(CRT,0,0,solid);
+    box(CRT,0,0,getwidth(CRT),getheight(CRT),0,0);
+
+    Quickfont(TheFont,'Britannic Bold',20);
+
+    centerx := getwidth(CRT) div 2;
+    centery := getheight(CRT) div 2;
+
+  for i:=20 downto 1 do
+    begin
+      radius:=i*12.5;
+      angle:=i*50;
+      x:=round(centerx+1.5*radius*cos(angle/180*pi));
+      y:=round(centery-radius*sin(angle/180*pi));
+      TheFont.angle := round(angle-90+360) mod 360;
+      TheFont.height  := round(2.5*i);
+      TheFont.fcolor := gradient(color[8],color[7],i,20);
+      print(CRT,x,y,'EasyCRT 4.0');
+      delay(100);
+    end;
+
+  end;
+
+procedure drawtitle;
+var path: string;
+    letters: array[1..7] of bmp;
+    lettersm: array[1..7] of bmp;
+    i,j,k,totalwidth,totalheight,x,y,w,h: integer;
+    fact:real;
+    b1,b2:bmp;
+    
+  begin
+
+    letters[1]  := loadbmp(CRT,appdir+'\e.bmp');
+    letters[2]  := loadbmp(CRT,appdir+'\a.bmp');
+    letters[3]  := loadbmp(CRT,appdir+'\s.bmp');
+    letters[4]  := loadbmp(CRT,appdir+'\y.bmp');
+    letters[5]  := loadbmp(CRT,appdir+'\c.bmp');
+    letters[6]  := loadbmp(CRT,appdir+'\r.bmp');
+    letters[7]  := loadbmp(CRT,appdir+'\t.bmp');
+
+    lettersm[1] := loadbmp(CRT,appdir+'\em.bmp');
+    lettersm[2] := loadbmp(CRT,appdir+'\am.bmp');
+    lettersm[3] := loadbmp(CRT,appdir+'\sm.bmp');
+    lettersm[4] := loadbmp(CRT,appdir+'\ym.bmp');
+    lettersm[5] := loadbmp(CRT,appdir+'\cm.bmp');
+    lettersm[6] := loadbmp(CRT,appdir+'\rm.bmp');
+    lettersm[7] := loadbmp(CRT,appdir+'\tm.bmp');
+
+    totalwidth := 0;
+    for i := 1 to 7 do
+      totalwidth := totalwidth + getwidth(letters[i]) - overlap;
+
+    totalheight := getheight(letters[1]);
+    x := (getpos(clientw)-totalwidth) div 2+totalwidth;
+    y := (getpos(clienth)-totalheight) div 2-50;
+
+    b1 := makeblankbmp(CRT,getwidth(CRT),getheight(CRT));
+    b2 := makeblankbmp(CRT,getwidth(CRT),getheight(CRT));
+
+    copybmp(CRT,b1,0,0);
+    copybmp(CRT,b2,0,0);
+
+    for i := 7 downto 1 do
+      begin
+        unfreeze;
+        h := totalheight;
+        w := getwidth(letters[i]);
+        x := x-w+overlap;
+        for j := 3 downto 1 do
+          begin
+            fact := pwrxy(j,1.3);
+            copybmp(b2,b1,0,0);
+            supremecopy(letters[i],lettersm[i],b1,
+              0,0,0,0,x,y,round(w*fact),round(h*fact));
+           copybmp(b1,CRT,0,0);  
+          end;
+        copybmp(b1,b2,0,0); 
+      end;
+
+    for i := 1 to 7 do
+      begin
+        killBMP(letters[i]);
+        killBMP(lettersm[i]);
+      end;
+
+    killbmp(b1);
+    killbmp(b2);
+
+
+    Quickfont(TheFont,times,20);
+    TheFont.halign := ta_center;
+    TheFont.bgcolor := 0;
+    resetkeys;
+    i := 1; j := 0;
+    repeat
+      if j>=50 then i:=-1;
+      if j<=0 then i:=1;
+      j:=j+i;
+      TheFont.fcolor := gradient(color[0],color[15],j,50);
+      print(CRT,getwidth(CRT) div 2,getheight(CRT) div 2+50,'by Russ Yanofsky');
+      delay(30);
+    until keypressed or ldown or rdown;
+
+  end;
+
+begin
+  initwincrt;
+  setborder(caption,0);
+  setborder(scrollbar,0);
+
+  easyspiral;
+  drawtitle;
+
+  donewincrt; 
+end.
