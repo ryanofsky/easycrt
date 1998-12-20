3b2a536799536f559d3fe64c2863635f30ba213b
Russell Yanofsky <russ@yanofsky.org>
1998-12-20 22:45:36 +0000

    - colors green when both buttons pressed

--- samples/emouse.pas
+++ samples/emouse.pas
@@ -10,7 +10,8 @@ begin
   repeat
     gotoxy(1,1); write('X':5,'   ','Y':5,'   ','LEFT':5,'   ','RIGHT':5);
     gotoxy(1,2); write(mousex:5,'   ',mousey:5,'   ',ldown:5,'   ',rdown:5);
-    if ldown then begin setbrush(CRT,color[4],0,solid); circle(CRT,mousex,mousey,3,3); end;
-    if rdown then begin setbrush(CRT,color[1],0,solid); circle(CRT,mousex,mousey,3,3); end;
+    if ldown and not rdown then begin setbrush(CRT,color[4],0,solid); circle(CRT,mousex,mousey,3,3); end;
+    if rdown and not ldown then begin setbrush(CRT,color[1],0,solid); circle(CRT,mousex,mousey,3,3); end;
+    if ldown   and   rdown then begin setbrush(CRT,color[2],0,solid); circle(CRT,mousex,mousey,3,3); end;
   until inkey<>0;
 end.
